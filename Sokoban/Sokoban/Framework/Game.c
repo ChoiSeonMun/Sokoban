c09e1bef94a6ebc60cdbbe24ea6e632a8d145f0d
seonmunchoi <seonmun.choi@gmail.com>
2022-03-31 11:33:04 +0900

    Implement input processing

--- Sokoban/Sokoban/Framework/Game.c
+++ Sokoban/Sokoban/Framework/Game.c
@@ -1,6 +1,7 @@
 #include "stdafx.h"
 #include "Game.h"
 #include "Renderer.h"
+#include "Input.h"
 
 bool Initialize()
 {
@@ -14,12 +15,27 @@ bool Initialize()
 
 void processInput()
 {
-
+	UpdateInput();
 }
 
 void update()
 {
-
+	if (GetButton(KEYCODE_W))
+	{
+		SetKeyMessage(KEYCODE_W);
+	}
+	else if (GetButton(KEYCODE_D))
+	{
+		SetKeyMessage(KEYCODE_D);
+	}
+	else if (GetButton(KEYCODE_S))
+	{
+		SetKeyMessage(KEYCODE_S);
+	}
+	else if (GetButton(KEYCODE_A))
+	{
+		SetKeyMessage(KEYCODE_A);
+	}
 }
 
 void render()
@@ -29,17 +45,12 @@ void render()
 
 int32_t Run()
 {
-	// Game Loop => 플레이어로부터의 입력을 하드웨어와 분리시킨다.
-	// https://gameprogrammingpatterns.com/game-loop.html
-
-	// Game Loop의 전체를 Frame
 	while (true)
 	{
-		// 입력 처리
 		processInput();
-		// 업데이트
 		update();
-		// 렌더링
 		render();
 	}
+
+	return 0;
 }
--- Sokoban/Sokoban/Framework/Renderer.c
+++ Sokoban/Sokoban/Framework/Renderer.c
@@ -3,12 +3,18 @@
 
 #define MAP_SIZE 24
 
-static char s_map[MAP_SIZE][MAP_SIZE] = {
-	"***********",
-	"* Sokoban *",
-	"***********"
-};
+static char s_map[MAP_SIZE][MAP_SIZE];
 static HANDLE s_consoleHandle;
+
+void clear()
+{
+	memset(s_map, ' ', sizeof(s_map));
+
+	for (size_t i = 0; i < MAP_SIZE; ++i)
+	{
+		s_map[i][MAP_SIZE - 1] = '\0';
+	}
+}
 
 bool InitializeRenderer()
 {
@@ -24,14 +30,22 @@ bool InitializeRenderer()
 
 void RenderMap()
 {
+	
 	const static COORD initialPos = { 0, 0 };
 	const static CONSOLE_CURSOR_INFO info = { 100, false };
 
 	SetConsoleCursorPosition(s_consoleHandle, initialPos);
 	SetConsoleCursorInfo(s_consoleHandle, &info);
 
-	for (int i = 0; i < MAP_SIZE; ++i)
+	for (size_t i = 0; i < MAP_SIZE; ++i)
 	{
 		puts(s_map[i]);
 	}
+
+	clear();
+}
+
+void SetKeyMessage(int32_t keyCode)
+{
+	sprintf_s(s_map[0], sizeof(s_map[0]), "%c키가 눌림", keyCode);
 }
